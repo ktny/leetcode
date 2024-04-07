e8119e7bc9a32d4e607230165872118cc7eb9916
ktny <katsunori.t10@gmail.com>
2024-04-07 16:45:05 +0900

    add 1431

---
+++ 1431_KidsWithTheGratestNumberOfCandies.py
@@ -0,0 +1,8 @@
+class Solution:
+    def kidsWithCandies(self, candies: list[int], extraCandies: int) -> list[bool]:
+        m = max(candies)
+        ans = []
+        for n in candies:
+            ans.append(n + extraCandies >= m)
+
+        return ans
