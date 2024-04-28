f159bec42f60e850a863b782cbf73958d571e842
ktny <katsunori.t10@gmail.com>
2024-04-28 23:37:33 +0900

    fix

--- Pandas/183.py
+++ Pandas/183.py
@@ -2,6 +2,12 @@ import pandas as pd
 
 
 def find_customers(customers: pd.DataFrame, orders: pd.DataFrame) -> pd.DataFrame:
-    df = customers.merge(orders, left_on="id", right_on="customerId", how="left")
-    df = df.loc[df["customerId"].isna(), ["name"]]
-    return df.rename(columns={"name": "Customers"})
+    df = customers[~customers["id"].isin(orders["customerId"])]
+    return df[["name"]].rename(columns={"name": "Customers"})
+
+
+# 別解
+# def find_customers(customers: pd.DataFrame, orders: pd.DataFrame) -> pd.DataFrame:
+#     df = customers.merge(orders, left_on="id", right_on="customerId", how="left")
+#     df = df.loc[df["customerId"].isna(), ["name"]]
+#     return df.rename(columns={"name": "Customers"})
